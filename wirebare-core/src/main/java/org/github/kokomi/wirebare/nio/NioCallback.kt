c95bd00c375fad7d6c538d689b549f963cb40a8d
Kokomi <939826156@qq.com>
2023-03-15 21:31:18 +0800

    完成对 NIO 的封装，包括 SocketChannel(TCP) 和 DatagramSocketChannel(UDP) 两个部分

---
+++ wirebare-core/src/main/java/org/github/kokomi/wirebare/nio/NioCallback.kt
@@ -0,0 +1,32 @@
+package org.github.kokomi.wirebare.nio
+
+interface NioCallback {
+
+    /**
+     * 连接完成时回调
+     * */
+    fun onConnected()
+
+    /**
+     * 接收到 Accept 事件时对调
+     * */
+    fun onAccept()
+
+    /**
+     * 接收到 Read 事件时对调
+     * */
+    fun onRead()
+
+    /**
+     * 接收到 Write 事件时对调
+     *
+     * @return 本次写入的字节总数
+     * */
+    fun onWrite(): Int
+
+    /**
+     * 关闭时回调
+     * */
+    fun onClosed()
+
+}
